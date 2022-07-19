5557c3498c792b9367b1239e6c5ddd015d327ac7
Mohan-verma <vermamohan825@gmail.com>
2022-07-19 17:03:56 +0530

    disabled otp for image testing

--- index.js
+++ index.js
@@ -168,31 +168,31 @@ app.post("/login", (req, res) => {
 
     console.log("number", number)
     res.send({ from: "login screens", message: number })
-    if (number.length >= 13) {
-        // console.log(req.body)
-        const number = req.body.number;
-        const chann = "sms";
-        client
-            .verify
-            .services(process.env.SECURITY)
-            .verifications
-            .create({ to: number, channel: chann })
-            // then statement twilio
-            .then(data => {
-                console.log(data)
-                res.send("otp sent")
-            })
-            // error login invalid number
-            .catch(err => {
+    // if (number.length >= 13) {
+    //     // console.log(req.body)
+    //     const number = req.body.number;
+    //     const chann = "sms";
+    //     client
+    //         .verify
+    //         .services(process.env.SECURITY)
+    //         .verifications
+    //         .create({ to: number, channel: chann })
+    //         // then statement twilio
+    //         .then(data => {
+    //             console.log(data)
+    //             res.send("otp sent")
+    //         })
+    //         // error login invalid number
+    //         .catch(err => {
 
-                res.status(400).send(err)
-            })
-    }
+    //             res.status(400).send(err)
+    //         })
+    // }
 
-    else {
-        console.log("number is invalid invalid format plz ensure that you have filled your country code as well as number")
-        res.send("number is invalid invalid format plz ensure that you have filled your country code as well as number")
-    }
+    // else {
+    //     console.log("number is invalid invalid format plz ensure that you have filled your country code as well as number")
+    //     res.send("number is invalid invalid format plz ensure that you have filled your country code as well as number")
+    // }
 
 })
 
@@ -205,51 +205,51 @@ app.post('/verify', (req, res) => {
 
     const codes = req.body.code;
 
-    client.verify.services(process.env.SECURITY)
-        .verificationChecks
-        .create({ to: number, code: codes })
-        .then(verification_check => {
+    // client.verify.services(process.env.SECURITY)
+    //     .verificationChecks
+    // .create({ to: number, code: codes })
+    // .then(verification_check => {
 
-            console.log("checking", verification_check)
+    // console.log("checking", verification_check)
 
-            if (verification_check.status === "pending")
-                res.status(401).send("invalid OTP generate otp again")
-
-
-            else {
+    // if (verification_check.status === "pending")
+    //     res.status(401).send("invalid OTP generate otp again")
 
 
-                const user = new User({
-                    phoneNo: req.body.number,
-                })
-                user.save()
-                    .then((resolve) => {
-                        console.log("usernew")
+    // else {
 
-                        res.status(201).send({ message: " number registered", data: resolve })
-                    })
-                    .catch((err) => {
-                        const error = err;
-                        console.log(error)
-                        User.find({ phoneNo: req.body.number }, function (err, data) {
 
-                            if (err) {
-                                console.log(err);
-                            }
-                            else {
-                                console.log("First function call : ", data[0]);
-                                console.log("we are here")
-                                const userdetail = data[0];
-                                res.status(200).send({ message: "User already exist ", userdetail })
-                                console.log("we are done")
-                            }
+    const user = new User({
+        phoneNo: req.body.number,
+    })
+    user.save()
+        .then((resolve) => {
+            console.log("usernew")
 
-                        });
+            res.status(201).send({ message: " number registered", data: resolve })
+        })
+        .catch((err) => {
+            const error = err;
+            console.log(error)
+            User.find({ phoneNo: req.body.number }, function (err, data) {
 
-                    })
-            }
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    console.log("First function call : ", data[0]);
+                    console.log("we are here")
+                    const userdetail = data[0];
+                    res.status(200).send({ message: "User already exist ", userdetail })
+                    console.log("we are done")
+                }
+
+            });
 
         })
+    // }
+
+    // })
 
     // console.log(codes)
 
